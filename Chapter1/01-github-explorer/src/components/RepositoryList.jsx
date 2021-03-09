b5fac33d8be55d4a50de12b23b8e1c7f9a3085ab
DiogoM14 <diogomartins200214@gmail.com>
2021-03-09 07:54:08 +0000

    HTTP call to github api

--- Chapter1/01-github-explorer/src/components/RepositoryList.jsx
+++ Chapter1/01-github-explorer/src/components/RepositoryList.jsx
@@ -1,20 +1,25 @@
+import { useState, useEffect } from 'react'
 import { RepositoryItem } from "./RepositoryItem"
 
-const repository = {
-  name: 'unform',
-  description: 'Forms in react',
-  link: 'https://github.com/diogom14'
-}
+import '../styles/repositories.scss'
 
 export function RepositoryList() {
+  const [repositories, setRepositories] = useState([])
+
+  useEffect(() => {
+    fetch('https://api.github.com/users/diogom14/repos')
+      .then(response => response.json())
+      .then(data => setRepositories(data))
+  }, [])
+
   return (
     <section className="repository-list">
       <h1>Lista de repositórios</h1>
 
       <ul>
-        <RepositoryItem repository={repository} />
-        <RepositoryItem repository={repository} />
-        <RepositoryItem repository={repository} />
+        {repositories.map(repository => {
+          return <RepositoryItem key={repository.name} repository={repository} />
+        })}
       </ul>
     </section>
   )
