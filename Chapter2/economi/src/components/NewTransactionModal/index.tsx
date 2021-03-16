705941ebd33528f5a509971abb3641ef7b750440
DiogoM14 <diogomartins200214@gmail.com>
2021-03-16 14:31:26 +0000

    Insert data in miragejs

--- Chapter2/economi/src/components/NewTransactionModal/index.tsx
+++ Chapter2/economi/src/components/NewTransactionModal/index.tsx
@@ -1,8 +1,9 @@
-import { useState } from 'react'
+import { FormEvent, useState } from 'react'
 import Modal from 'react-modal'
 import closeImg from '../../assets/close.svg'
 import incomeImage from '../../assets/income.svg'
 import outcomeImage from '../../assets/outcome.svg'
+import { api } from '../../services/api'
 import { Container, RadioBox, TransactionTypeContainer } from './styles'
 
 interface NewTransactionModalProps {
@@ -11,7 +12,20 @@ interface NewTransactionModalProps {
 }
 
 export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModalProps) {
+  const [title, setTitle] = useState('')
+  const [value, setValue] = useState(0)
+  const [category, setCategory] = useState('')
   const [type, setType] = useState('')
+
+  function handleCreateNewTransaction(event: FormEvent) {
+    event.preventDefault()
+
+    const data = {
+      title, value, type, category
+    }
+
+    api.post('transactions', data)
+  }
 
   return (
   <Modal
@@ -25,16 +39,20 @@ export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModa
       <img src={closeImg} alt="Fechar modal"/>
     </button>
 
-    <Container>
+    <Container onSubmit={handleCreateNewTransaction}>
       <h2>Registar transação</h2>
 
       <input 
         placeholder="Título"
+        value={title}
+        onChange={event => setTitle(event.target.value)}
       />
 
       <input 
         type="number"
         placeholder="Valor"
+        value={value}
+        onChange={event => setValue(Number(event.target.value))}
       />
 
       <TransactionTypeContainer>
@@ -61,6 +79,8 @@ export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModa
 
       <input 
         placeholder="Categoria"
+        value={category}
+        onChange={event => setCategory(event.target.value)}
       />
 
       <button type="submit">Registar</button>
