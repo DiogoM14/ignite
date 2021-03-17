c2ef5e123f5e93a8e4f309d3adb3dd4729c42e68
DiogoM14 <diogomartins200214@gmail.com>
2021-03-17 11:50:03 +0000

    Displaying api data

--- Chapter2/economi/src/components/NewTransactionModal/index.tsx
+++ Chapter2/economi/src/components/NewTransactionModal/index.tsx
@@ -17,17 +17,23 @@ export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModa
   const [title, setTitle] = useState('')
   const [amount, setAmount] = useState(0)
   const [category, setCategory] = useState('')
-  const [type, setType] = useState('')
+  const [type, setType] = useState('deposit')
 
-  function handleCreateNewTransaction(event: FormEvent) {
+  async function handleCreateNewTransaction(event: FormEvent) {
     event.preventDefault()
 
-    createTransaction({
+    await createTransaction({
       title,
       amount,
       category,
       type
     })
+
+    setTitle('')
+    setAmount(0)
+    setCategory('')
+    setType('')
+    onRequestClose()
   }
 
   return (
