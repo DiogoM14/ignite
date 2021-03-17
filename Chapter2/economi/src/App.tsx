bb37db3ba50a78ff52d5f96d10b2af6729c6fd64
DiogoM14 <diogomartins200214@gmail.com>
2021-03-17 11:41:56 +0000

    Using context api

--- Chapter2/economi/src/App.tsx
+++ Chapter2/economi/src/App.tsx
@@ -6,6 +6,7 @@ import { Header } from "./components/Header"
 import { NewTransactionModal } from './components/NewTransactionModal'
 
 import { GlobalStyle } from "./styles/global"
+import { TransactionsProvider } from './TransactionsContext'
 
 Modal.setAppElement('#root')
 
@@ -21,7 +22,7 @@ export function App() {
   }
   
   return (
-    <>
+    <TransactionsProvider>
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
 
@@ -30,6 +31,6 @@ export function App() {
         onRequestClose={handleCloseNewTransactionModal}
       />
       <GlobalStyle />
-    </>
+    </TransactionsProvider>
   )
 }
--- Chapter2/economi/src/components/NewTransactionModal/index.tsx
+++ Chapter2/economi/src/components/NewTransactionModal/index.tsx
@@ -1,9 +1,9 @@
-import { FormEvent, useState } from 'react'
+import { FormEvent, useState, useContext } from 'react'
 import Modal from 'react-modal'
 import closeImg from '../../assets/close.svg'
 import incomeImage from '../../assets/income.svg'
 import outcomeImage from '../../assets/outcome.svg'
-import { api } from '../../services/api'
+import { TransactionsContext } from '../../TransactionsContext'
 import { Container, RadioBox, TransactionTypeContainer } from './styles'
 
 interface NewTransactionModalProps {
@@ -12,19 +12,22 @@ interface NewTransactionModalProps {
 }
 
 export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModalProps) {
+  const { createTransaction } = useContext(TransactionsContext)
+
   const [title, setTitle] = useState('')
-  const [value, setValue] = useState(0)
+  const [amount, setAmount] = useState(0)
   const [category, setCategory] = useState('')
   const [type, setType] = useState('')
 
   function handleCreateNewTransaction(event: FormEvent) {
     event.preventDefault()
 
-    const data = {
-      title, value, type, category
-    }
-
-    api.post('transactions', data)
+    createTransaction({
+      title,
+      amount,
+      category,
+      type
+    })
   }
 
   return (
@@ -51,8 +54,8 @@ export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModa
       <input 
         type="number"
         placeholder="Valor"
-        value={value}
-        onChange={event => setValue(Number(event.target.value))}
+        value={amount}
+        onChange={event => setAmount(Number(event.target.value))}
       />
 
       <TransactionTypeContainer>
