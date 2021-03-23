7ced82851ddc5ab105d022ff9eb8eea88e79df97
DiogoM14 <diogomartins200214@gmail.com>
2021-03-23 16:03:10 +0000

    Adding SSG

--- Chapter3/ignews/src/pages/index.tsx
+++ Chapter3/ignews/src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps } from 'next'
+import { GetStaticProps } from 'next'
 import Head from 'next/head'
 
 import { stripe } from '../services/stripe'
@@ -36,7 +36,7 @@ export default function Home({ product }: HomeProps) {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve('price_1IYC0vCkS1OP5qkjIeJ1t6kF')
 
   const product = {
@@ -50,6 +50,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
   return {
     props: {
       product
-    }
+    },
+    revalidate: 60 * 60 * 24  // 24 hours
   }
 }
