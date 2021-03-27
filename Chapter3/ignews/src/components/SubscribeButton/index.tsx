7224557cc93ea9d34c5c25a29c5b8060eff15e0b
DiogoM14 <diogomartins200214@gmail.com>
2021-03-27 17:33:18 +0000

    Redirecting to Stripe

--- Chapter3/ignews/src/components/SubscribeButton/index.tsx
+++ Chapter3/ignews/src/components/SubscribeButton/index.tsx
@@ -1,4 +1,6 @@
 import { useSession, signIn } from 'next-auth/client'
+import { api } from '../../services/api'
+import { getStripeJS } from '../../services/stripe-js'
 import styles from './styles.module.scss'
 
 interface SubscribeButtonProps {
@@ -8,13 +10,24 @@ interface SubscribeButtonProps {
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
   const [ session ] = useSession()
 
-  function handleSubscribe() {
+  async function handleSubscribe() {
     if (!session) {
       signIn('github')
       return
     }
 
     // criação do checkout
+    try {
+      const response = await api.post('subscribe')
+
+      const { sessionId } = response.data
+
+      const stripe = await getStripeJS()
+
+      await stripe.redirectToCheckout({ sessionId }) 
+      } catch (err) {
+        alert(err.message)
+      }
   }
 
   return (
