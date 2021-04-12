b03afabc6b7cc4b0a0159cf65f686f68e901aaa9
DiogoM14 <diogomartins200214@gmail.com>
2021-04-12 14:14:42 +0100

    Users table

---
+++ Chapter4/dash.go/src/pages/users/index.tsx
@@ -0,0 +1,75 @@
+import { Button } from "@chakra-ui/button";
+import { Checkbox } from "@chakra-ui/checkbox";
+import Icon from "@chakra-ui/icon";
+import { Box, Flex, Heading, Text } from "@chakra-ui/layout";
+import { Table, Tbody, Td, Th, Thead, Tr } from "@chakra-ui/table";
+import { RiAddLine, RiPencilLine } from "react-icons/ri";
+import { Header } from "../../components/Header";
+import { Sidebar } from "../../components/Sidebar";
+
+export default function UserList() {
+  return (
+    <Box>
+      <Header />
+      <Flex w="100%" my="6" maxW={1480} mx="auto" px="6">
+        <Sidebar />
+
+        <Box flex="1" borderRadius={8} bg="gray.800" p="8">
+          <Flex mb="8" justify="space-between" align="center">
+            <Heading size="lg" fontWeight="normal">Utilizadores</Heading>
+
+            <Button 
+              as="a" 
+              size="sm" 
+              fontSize="sm"  
+              colorScheme="pink" 
+              leftIcon={<Icon as={RiAddLine} fontSize="20" />}
+            >
+              Criar novo
+            </Button>
+          </Flex>
+
+          <Table colorScheme="whiteAlpha">
+            <Thead>
+              <Tr>
+                <Th px="6" color="gray.300" width="8">
+                  <Checkbox colorScheme="pink" />
+                </Th>
+                <Th>Utilizador</Th>
+                <Th>Data de Registo</Th>
+                <Th w="8"></Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              <Tr>
+                <Td px="6">
+                  <Checkbox colorScheme="pink" />
+                </Td>
+                <Td>
+                  <Box>
+                    <Text fontWeight="bold">Diogo Martins</Text>
+                    <Text fontSize="sm" color="gray.300">diogomartins200214@gmail.com</Text>
+                  </Box>
+                </Td>
+                <Td>
+                  04 de abril, 2021
+                </Td>
+                <Td>
+                  <Button 
+                    as="a" 
+                    size="sm" 
+                    fontSize="sm" 
+                    colorScheme="pink" 
+                    leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
+                  >
+                    Editar
+                  </Button>
+                </Td>
+              </Tr>
+            </Tbody>
+          </Table>
+        </Box>
+      </Flex>
+    </Box>
+  )
+}
