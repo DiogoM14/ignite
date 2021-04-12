e8059cd31a3a95999acf1e075777ef8175607904
DiogoM14 <diogomartins200214@gmail.com>
2021-04-12 08:27:01 +0100

    Header component/search bar

--- Chapter4/dash.go/src/pages/index.tsx
+++ Chapter4/dash.go/src/pages/index.tsx
@@ -1,6 +1,7 @@
-import { Flex, Input, Button, Stack, FormLabel, FormControl } from '@chakra-ui/react'
+import { Flex, Button, Stack } from '@chakra-ui/react'
+import { Input } from '../components/Form'
 
-export default function Home() {
+export default function SignIn() {
   return (
     <Flex 
       w="100vw" 
@@ -18,37 +19,8 @@ export default function Home() {
         flexDir="column"
       >
         <Stack spacing="4">
-          <FormControl>
-            <FormLabel htmlFor="email">E-mail</FormLabel>
-
-            <Input 
-              name="email" 
-              type="email" 
-              id="email"
-              focusBorderColor="pink.500"
-              bgColor="gray.900"
-              variant="filled"
-              _hover={{ 
-                bgColor: 'gray.900'
-              }}
-              size="lg"
-            />
-
-            <FormLabel htmlFor="password">Password</FormLabel>
-
-            <Input 
-              name="password" 
-              type="password" 
-              id="password"
-              focusBorderColor="pink.500"
-              bgColor="gray.900"
-              variant="filled"
-              _hover={{ 
-                bgColor: 'gray.900'
-              }}
-              size="lg"
-            />
-          </FormControl>
+         <Input name="email" type="email" label="E-mail" />
+         <Input name="password" type="password" label="Password" />
         </Stack>
 
         <Button 
