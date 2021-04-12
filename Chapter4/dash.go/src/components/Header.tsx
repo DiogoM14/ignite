658344f1578de0acf32a0db962cd849ae0644a63
DiogoM14 <diogomartins200214@gmail.com>
2021-04-12 08:37:17 +0100

    Header component/profile

--- Chapter4/dash.go/src/components/Header.tsx
+++ Chapter4/dash.go/src/components/Header.tsx
@@ -1,5 +1,5 @@
-import { Flex, Text, Input, Icon } from '@chakra-ui/react'
-import { RiSearchLine } from 'react-icons/ri'
+import { Flex, Text, Input, Icon, HStack, Box, Avatar } from '@chakra-ui/react'
+import { RiSearchLine, RiNotificationLine, RiUserAddLine } from 'react-icons/ri'
 
 export function Header() {
   return (
@@ -47,6 +47,36 @@ export function Header() {
         />
 
         <Icon as={RiSearchLine} fontSize="20" />
+      </Flex>
+
+      <Flex
+        align="center"
+        ml="auto"
+      >
+        <HStack 
+          spacing="4"
+          mx="8"
+          pr="8"
+          py="1"
+          color="gray.300"
+          borderRightWidth={1}
+          borderColor="gray.700"
+        >
+          <Icon as={RiNotificationLine} fontSize="20" />
+          <Icon as={RiUserAddLine} fontSize="20" />
+        </HStack>
+        <Flex
+          align="center"
+        >
+          <Box mr="4" textAlign="right">
+            <Text>Diogo Martins</Text>
+            <Text color="gray.300" fontSize="small">
+              diogomartins200214@gmail.com
+            </Text>
+          </Box>
+
+          <Avatar size="md" name="Diogo Martins" src="https://github.com/diogom14.png" />
+        </Flex>
       </Flex>
     </Flex>
   )
--- Chapter4/dash.go/src/pages/index.tsx
+++ Chapter4/dash.go/src/pages/index.tsx
@@ -1,5 +1,5 @@
 import { Flex, Button, Stack } from '@chakra-ui/react'
-import { Input } from '../components/Form'
+import { Input } from '../components/Form/Input'
 
 export default function SignIn() {
   return (
