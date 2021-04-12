e40227f156e05041dbec7844abdf89dc2077f65f
DiogoM14 <diogomartins200214@gmail.com>
2021-04-12 14:52:24 +0100

    Finishing create user page

---
+++ Chapter4/dash.go/src/pages/users/create.tsx
@@ -0,0 +1,40 @@
+import { Box, Divider, Flex, Heading, VStack, SimpleGrid, HStack, Button } from "@chakra-ui/react";
+import { Input } from "../../components/Form/Input";
+import { Header } from "../../components/Header";
+import { Sidebar } from "../../components/Sidebar";
+
+export default function CreateUser() {
+  return (
+    <Box>
+      <Header />
+      <Flex w="100%" my="6" maxW={1480} mx="auto" px="6">
+        <Sidebar />
+
+        <Box flex="1" borderRadius={8} bg="gray.800" p="8">
+          <Heading size="lg" fontWeight="normal">Criar Utilizador</Heading>
+
+          <Divider my="6" borderColor="gray.700" />
+
+          <VStack spacing="8">
+            <SimpleGrid minChildWidth="240px" spacing="8" w="100%">
+              <Input name="name" label="Nome Completo" />
+              <Input name="email" type="email" label="E-mail" />
+            </SimpleGrid>
+
+            <SimpleGrid minChildWidth="240px" spacing="8" w="100%">
+              <Input name="password" type="password" label="Password" />
+              <Input name="password_confirmation" type="password" label="Confirmação da Password" />
+            </SimpleGrid>
+          </VStack>
+
+          <Flex mt="8" justify="flex-end">
+            <HStack spacing="4">
+              <Button colorScheme="whiteAlpha">Cancelar</Button>
+              <Button colorScheme="pink">Guardar</Button>
+            </HStack>
+          </Flex>
+        </Box>
+      </Flex>
+    </Box>
+  )
+}
