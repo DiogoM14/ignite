0700ea062fad664b94728677e4d14a1b6ffca1fe
DiogoM14 <diogomartins200214@gmail.com>
2021-08-20 22:42:14 +0100

    Saving jwt with cookies

--- Chapter4/reactjs-auth-frontend/contexts/AuthContext.tsx
+++ Chapter4/reactjs-auth-frontend/contexts/AuthContext.tsx
@@ -1,5 +1,6 @@
-import Router from "next/dist/client/router"
 import { createContext, ReactNode, useState } from "react"
+import { setCookie } from 'nookies'
+import Router from "next/dist/client/router"
 import { api } from "../services/api"
 
 type User = {
@@ -36,7 +37,17 @@ export function AuthProvider({ children }: AuthProviderProps) {
         password
       })
 
-      const { permissions, roles } = response.data
+      const { token, refreshToken, permissions, roles } = response.data
+
+      setCookie(undefined, 'nextauth.token', token, {
+        maxAge: 60 * 60 * 24 * 30, // 30 days
+        path: '/'
+      })
+      
+      setCookie(undefined, 'nextauth.refreshToken', refreshToken, {
+        maxAge: 60 * 60 * 24 * 30, // 30 days
+        path: '/'
+      })
   
       setUser({
         email,
