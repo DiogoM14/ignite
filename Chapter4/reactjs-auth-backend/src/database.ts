eea37be48a4f3657a729a27e36f4348406b398eb
DiogoM14 <diogomartins200214@gmail.com>
2021-08-21 14:15:23 +0100

    Doing token's refresh

--- Chapter4/reactjs-auth-backend/src/database.ts
+++ Chapter4/reactjs-auth-backend/src/database.ts
@@ -6,7 +6,7 @@ export const users: UsersStore = new Map()
 export const tokens: RefreshTokensStore = new Map()
 
 export function seedUserStore() {
-  users.set('diego@rocketseat.team', {
+  users.set('diogo@agil.team', {
     password: '123456',
     permissions: ['users.list', 'users.create', 'metrics.list'],
     roles: ['administrator']
