cf72def69ed08a85befb27c6521ded4547d8bedd
DiogoM14 <diogomartins200214@gmail.com>
2021-09-09 17:11:43 +0100

    Guest Validation

--- Chapter4/reactjs-auth-frontend/pages/index.tsx
+++ Chapter4/reactjs-auth-frontend/pages/index.tsx
@@ -2,6 +2,7 @@ import { GetServerSideProps } from "next"
 import { parseCookies } from "nookies"
 import { FormEvent, useContext, useState } from "react"
 import { AuthContext } from "../contexts/AuthContext"
+import { withSSRGuest } from "../utils/withSSRGuest"
 
 export default function Home() {
   const [ email, setEmail ] = useState('')
@@ -29,21 +30,10 @@ export default function Home() {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async (ctx) => {
-  // console.log(ctx.req.cookies)
+export const getServerSideProps = withSSRGuest(async (ctx) => {
+  return {
+    props: {
 
-  const cookies = parseCookies(ctx)
-
-  if (cookies['nextauth.token']) {
-    return {
-      redirect: {
-        destination: '/dashboard',
-        permanent: false
-      }
     }
   }
-
-  return {
-    props: {}
-  }
-}
+})
