a45de5df0e55e64d1ad2f6ba43c2f8f3d800c50c
DiogoM14 <diogomartins200214@gmail.com>
2021-09-16 22:42:53 +0100

    Permissions hook

--- Chapter4/reactjs-auth-frontend/pages/dashboard.tsx
+++ Chapter4/reactjs-auth-frontend/pages/dashboard.tsx
@@ -3,9 +3,14 @@ import { AuthContext } from "../contexts/AuthContext"
 import { withSSRAuth } from "../utils/withSSRAuth"
 import { setupAPIClient } from "../services/api"
 import { api } from "../services/apiClient"
+import { useCan } from "../hooks/useCan"
 
 export default function Dashboard() {
   const { user } = useContext(AuthContext)
+
+  const userCanSeeMetrics = useCan({
+    permissions: ['metrics.list']
+  })
 
   useEffect(() => {
     api.get('/me')
@@ -14,7 +19,11 @@ export default function Dashboard() {
   }, [])
 
   return (
-    <h1>Dashboard { user?.email }</h1>
+    <>
+      <h1>Dashboard { user?.email }</h1>
+
+      { userCanSeeMetrics && <div>Métricas</div> }
+    </>
   )
 }
 
